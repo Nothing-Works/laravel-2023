1a4bd5774d145653ba6c4749b804077ad4175648
AndySong <andylauszp@gmail.com>
2023-06-11 00:03:13 +1200

    wip

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -13,23 +13,22 @@
 
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>Category
-                </option>
-                <option value="personal">Personal</option>
-                <option value="business">Business</option>
-            </select>
-
-            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                 height="22" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#222"
-                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-                </g>
-            </svg>
+        <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
+            <x-dropdown>
+                <x-slot name="trigger">
+                    <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
+                        {{isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
+                        <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
+                    </button>
+                </x-slot>
+                <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+                @foreach($categories as $category)
+                    <x-dropdown-item
+                        href="/categories/{{$category->slug}}"
+                        :active='request()->is("categories/{$category->slug}")'
+                    >{{ucwords($category->name)}}</x-dropdown-item>
+                @endforeach
+            </x-dropdown>
         </div>
 
         <!-- Other Filters -->
--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,27 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts', ['posts' => Post::latest()->with('category', 'author')->get()]);
-});
+    return view('posts', [
+        'posts' => Post::latest()->with('category', 'author')->get(),
+        'categories' => Category::all()
+    ]);
+})->name('home');
 
 Route::get('/posts/{post}', function (Post $post) {
     return view('post', ['post' => $post]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', ['posts' => $category->posts->load(['category', 'author'])]);
-});
+    return view('posts', [
+        'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
+        'categories' => Category::all()
+    ]);
+})->name('category');
 
 Route::get('/authors/{author:username}', function (User $author) {
-    return view('posts', ['posts' => $author->posts->load(['category', 'author'])]);
+    return view('posts', [
+        'posts' => $author->posts->load(['category', 'author']),
+        'categories' => Category::all()
+    ]);
 });

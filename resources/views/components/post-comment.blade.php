74ff52b20e9230aa77986b2b2ee4c55caa8b2253
AndySong <andylauszp@gmail.com>
2023-06-17 23:53:37 +1200

    wip

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,19 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci,
+            deserunt id nemo odit quaerat voluptate. Excepturi porro, sed? Dolor necessitatibus,
+            quo! Corporis doloribus est et expedita, ipsum repellendus veritatis?
+        </p>
+    </div>
+</article>
